362d436d28e622a9f3e874b7466d4c9082b3acae
Weerasak Chongnguluam <weerasak@Weerasaks-Omise-MacBook-Pro.local>
2018-11-29 11:59:25 +0700

    Delete and Update handler

--- main.go
+++ main.go
@@ -88,6 +88,41 @@ func (s *Server) GetByID(c *gin.Context) {
 	c.JSON(http.StatusOK, todo)
 }
 
+func (s *Server) DeleteByID(c *gin.Context) {
+	stmt := "DELETE FROM todos WHERE id = $1"
+	id, _ := strconv.Atoi(c.Param("id"))
+	_, err := s.db.Exec(stmt, id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+}
+
+func (s *Server) Update(c *gin.Context) {
+	h := gin.H{}
+	if err := c.ShouldBindJSON(&h); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		return
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	stmt := "UPDATE todos SET todo = $2 WHERE id = $1"
+	_, err := s.db.Exec(stmt, id, h["todo"])
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	stmt = "SELECT id, todo, created_at, updated_at FROM todos WHERE id = $1"
+	row := s.db.QueryRow(stmt, id)
+	var todo Todo
+	err = row.Scan(&todo.ID, &todo.Body, &todo.CreatedAt, &todo.UpdatedAt)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, todo)
+}
+
 func main() {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
@@ -113,8 +148,8 @@ func main() {
 	r.POST("/todos", s.Create)
 
 	r.GET("/todos/:id", s.GetByID)
-	// r.PUT("/todos/:id", s.Update)
-	// r.DELETE("/todos/:id", s.DeleteByID)
+	r.PUT("/todos/:id", s.Update)
+	r.DELETE("/todos/:id", s.DeleteByID)
 
 	r.Run(":" + os.Getenv("PORT"))
 }
